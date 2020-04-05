2ac012ba0967ce08bdcc690b2be6a739732bbb04
Artem Orlov <art.orlov@mail.ru>
2020-04-06 01:29:32 +0500

    Updates

---
+++ precache-manifest.61a472a9706163aac78e0aff057c8798.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "782424d4a0ad84c3b03f98de0ef59498",
+    "url": "/vkapp/index.html"
+  },
+  {
+    "revision": "d93772f30592a209e876",
+    "url": "/vkapp/static/css/2.4fc5a6bc.chunk.css"
+  },
+  {
+    "revision": "fd22811d6fb999bea224",
+    "url": "/vkapp/static/css/main.859eecb2.chunk.css"
+  },
+  {
+    "revision": "d93772f30592a209e876",
+    "url": "/vkapp/static/js/2.e6abd11a.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/vkapp/static/js/2.e6abd11a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd22811d6fb999bea224",
+    "url": "/vkapp/static/js/main.67cc8754.chunk.js"
+  },
+  {
+    "revision": "561bd8774258af0990b6",
+    "url": "/vkapp/static/js/runtime-main.e3115d03.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vkapp/static/media/persik.4e1ec840.png"
+  }
+]);
