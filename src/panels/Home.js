cf42ed3445a4ee2fb8008361359f56afff06aec3
orlove <orlov@orlov-pc2>
2020-04-06 14:09:12 +0500

    rm ngrok

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
-import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
-import Button from '@vkontakte/vkui/dist/components/Button/Button';
-import Group from '@vkontakte/vkui/dist/components/Group/Group';
-import { Header, InfoRow } from '@vkontakte/vkui';
-import Cell from '@vkontakte/vkui/dist/components/Cell/Cell';
-import Div from '@vkontakte/vkui/dist/components/Div/Div';
-import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar';
+import { Header, Div, Group, Button, Panel, PanelHeader } from '@vkontakte/vkui';
 
 import './Home.css';
 
